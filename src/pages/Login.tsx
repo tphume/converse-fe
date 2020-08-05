fc34cd3aa7eb7e46deaabc8c6204b5e160abb3b0
Traphume <traphumedev@gmail.com>
2020-08-05 16:55:41 +0700

    Align hero section and forms section

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -13,25 +13,43 @@ const useStyles = makeStyles((theme: Theme) => {
     container: {
       height: "100%",
     },
-    box: {
-      margin: "33% auto",
+    hero: {
+      height: "100%",
+      paddingTop: "15%",
+      backgroundColor: theme.palette.primary.main,
+      boxShadow: "4px 0 6px 4px hsla(0, 0%, 0%, 0.2)",
+    },
+    heroBox: {
+      height: "100%",
+      margin: "auto",
       maxWidth: "400px",
       display: "flex",
       flexDirection: "column",
     },
-    hero: {
-      backgroundColor: theme.palette.primary.main,
-      boxShadow: "4px 0 6px 4px hsla(0, 0%, 0%, 0.2)",
-    },
     heroHeader: {
       color: "white",
       fontWeight: 700,
+      marginBottom: "5px",
     },
     heroDesc: {
-      marginTop: "2px",
       color: "white",
       fontWeight: 400,
       lineHeight: 1.4,
+    },
+    form: {
+      height: "100%",
+      paddingTop: "15%",
+    },
+    formBox: {
+      height: "100%",
+      margin: "auto",
+      maxWidth: "500px",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "flex-start",
+    },
+    formHeader: {
+      marginBottom: "5px",
     },
   });
 });
@@ -44,7 +62,7 @@ export default function Login() {
     <section>
       <Grid container classes={{ container: classes.container }}>
         <Grid item sm={5} classes={{ item: classes.hero }}>
-          <div className={classes.box}>
+          <div className={classes.heroBox}>
             <Typography
               variant="h3"
               component="h1"
@@ -59,11 +77,17 @@ export default function Login() {
             </Typography>
           </div>
         </Grid>
-        <Grid item sm={7}>
-          <Typography variant="h4" component="h1">
-            Welcome Aboard
-          </Typography>
-          <Button onClick={() => dispatch(login("Phume"))}>Login</Button>
+        <Grid item sm={7} classes={{ item: classes.form }}>
+          <div className={classes.formBox}>
+            <Typography
+              variant="h4"
+              component="h1"
+              className={classes.formHeader}
+            >
+              Welcome Aboard
+            </Typography>
+            <Button onClick={() => dispatch(login("Phume"))}>Login</Button>
+          </div>
         </Grid>
       </Grid>
     </section>
