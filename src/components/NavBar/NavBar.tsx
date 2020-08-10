56b80971108a9665946116f366d31ae64a941141
Traphume <traphumedev@gmail.com>
2020-08-10 17:33:05 +0700

    Added color to navbar on current page

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { NavLink, LinkProps } from "react-router-dom";
+import { NavLink, LinkProps, useLocation } from "react-router-dom";
+
 import { Theme, createStyles, makeStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import Divider from "@material-ui/core/Divider";
+import Home from "@material-ui/icons/Home";
 import People from "@material-ui/icons/People";
 import Message from "@material-ui/icons/Message";
 import Help from "@material-ui/icons/Help";
@@ -15,14 +17,19 @@ const useListStyles = makeStyles((theme: Theme) => {
   return createStyles({
     container: {
       alignItems: "flex-end",
-      padding: "4px 8px 4px 8px",
+      padding: "6px 8px 6px 8px",
       borderRadius: "3px",
     },
     icon: {
       minWidth: "30px",
     },
     text: {
-      fontSize: "0.95em",
+      fontSize: "1em",
+      fontWeight: 600,
+    },
+    textColor: {
+      color: theme.palette.primary.main,
+      fontSize: "1em",
       fontWeight: 600,
     },
     textBox: {
@@ -37,8 +44,12 @@ interface ListItemLinkProps {
   to: string;
 }
 
-function ListItemLink(props: ListItemLinkProps) {
-  const { icon, primary, to } = props;
+interface ListProps extends ListItemLinkProps {
+  current: string;
+}
+
+function ListItemLink(props: ListProps) {
+  const { icon, primary, to, current } = props;
 
   const renderLink = React.useMemo(
     () =>
@@ -49,6 +60,7 @@ function ListItemLink(props: ListItemLinkProps) {
   );
 
   const classes = useListStyles();
+  const color = setTextColor(current, to);
 
   return (
     <li>
@@ -62,7 +74,10 @@ function ListItemLink(props: ListItemLinkProps) {
         ) : null}
         <ListItemText
           primary={primary}
-          classes={{ primary: classes.text, root: classes.textBox }}
+          classes={{
+            primary: color ? classes.textColor : classes.text,
+            root: classes.textBox,
+          }}
         />
       </ListItem>
     </li>
@@ -93,28 +108,71 @@ const useStyles = makeStyles((theme: Theme) => {
 
 export default function NavBar() {
   const classes = useStyles();
+  const location = useLocation();
 
   return (
     <nav className={classes.root}>
       <List className={classes.container}>
         <ListItemLink
+          to="/"
+          current={location.pathname}
+          primary="Home"
+          icon={
+            <Home
+              classes={{ root: classes.svg }}
+              color={setIconColor("/", location.pathname)}
+            />
+          }
+        />
+        <Divider className={classes.break} />
+        <ListItemLink
           to="/friends"
+          current={location.pathname}
           primary="Friends"
-          icon={<People classes={{ root: classes.svg }} />}
+          icon={
+            <People
+              classes={{ root: classes.svg }}
+              color={setIconColor("/friends", location.pathname)}
+            />
+          }
         />
         <Divider className={classes.break} />
         <ListItemLink
           to="/messages"
+          current={location.pathname}
           primary="Messages"
-          icon={<Message classes={{ root: classes.svg }} />}
+          icon={
+            <Message
+              classes={{ root: classes.svg }}
+              color={setIconColor("/messages", location.pathname)}
+            />
+          }
         />
         <Divider className={classes.break} />
         <ListItemLink
           to="/help"
+          current={location.pathname}
           primary="Help"
-          icon={<Help classes={{ root: classes.svg }} />}
+          icon={
+            <Help
+              classes={{ root: classes.svg }}
+              color={setIconColor("/help", location.pathname)}
+            />
+          }
         />
       </List>
     </nav>
   );
+}
+
+// Helper function to set colors
+function setTextColor(current: string, route: string): boolean {
+  return current === route ? true : false;
+}
+
+function setIconColor(
+  current: string,
+  route: string
+): "inherit" | "primary" | "secondary" | "action" | "disabled" | "error" {
+  return current === route ? "primary" : "inherit";
 }
