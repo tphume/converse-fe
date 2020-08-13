a8c3d022ea61ec3c5ed9de55cec91ab776129ccd
Traphume <traphumedev@gmail.com>
2020-08-13 10:50:45 +0700

    Fixed shadows

--- src/components/BottomNav/BottomNav.tsx
+++ src/components/BottomNav/BottomNav.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useLocation, useHistory } from "react-router-dom";
 
+import { Theme, createStyles, makeStyles } from "@material-ui/core/styles";
 import BottomNavigation from "@material-ui/core/BottomNavigation";
 import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
 import Home from "@material-ui/icons/Home";
@@ -8,7 +9,16 @@ import People from "@material-ui/icons/People";
 import ChatBubble from "@material-ui/icons/ChatBubble";
 import Help from "@material-ui/icons/Help";
 
+const useStyles = makeStyles((theme: Theme) => {
+  return createStyles({
+    container: {
+      boxShadow: "0px 4px 5px 3px hsla(0, 0%, 0%, 0.2)",
+    },
+  });
+});
+
 export default function BottomNav() {
+  const classes = useStyles();
   const location = useLocation();
   const history = useHistory();
 
@@ -17,7 +27,11 @@ export default function BottomNav() {
   };
 
   return (
-    <BottomNavigation value={location.pathname} onChange={handleChange}>
+    <BottomNavigation
+      value={location.pathname}
+      onChange={handleChange}
+      className={classes.container}
+    >
       <BottomNavigationAction label="Home" value="/" icon={<Home />} />
       <BottomNavigationAction
         label="Friends"
