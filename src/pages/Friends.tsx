319564d74d70c551f608db63f46e6dee46c72d8b
Traphume <traphumedev@gmail.com>
2020-08-19 18:59:23 +0700

    Replaced list with cards

--- src/pages/Friends.tsx
+++ src/pages/Friends.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../rootReducer";
 
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
 import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 
 export default function Friends(): JSX.Element {
   const { friends, loading } = useSelector((state: RootState) => state.friends);
@@ -26,17 +30,19 @@ export default function Friends(): JSX.Element {
 
   return (
     <section>
-      <Typography variant="h2">You are at Friends page</Typography>
-      <ul>
-        {friends.map((friend) => {
-          return (
-            <li key={friend.id}>
-              <Typography>{friend.username}</Typography>
-              <Typography>{friend.status}</Typography>
-            </li>
-          );
-        })}
-      </ul>
+      {friends.map((friend) => {
+        return (
+          <Card key={friend.id}>
+            <CardContent>
+              <Typography color="primary">{friend.username}:</Typography>
+              <Typography>"{friend.status}"</Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">message</Button>
+            </CardActions>
+          </Card>
+        );
+      })}
     </section>
   );
 }
