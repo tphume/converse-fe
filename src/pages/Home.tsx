f4613130c1548422168f7c90c3457efcd028c29c
Traphume <traphumedev@gmail.com>
2020-08-20 18:13:41 +0700

    Restyle home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,9 @@ import { logout } from "../features/user/user";
 
 import { Theme, createStyles, makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 
@@ -20,14 +23,12 @@ const useStyles = makeStyles((theme: Theme) => {
         justifyContent: "center",
       },
     },
-    header: {
-      marginTop: "2em",
+    card: {
+      borderColor: theme.palette.primary.main,
+      marginTop: "5em",
       [theme.breakpoints.down("xs")]: {
-        margin: 0,
+        margin: "auto",
       },
-    },
-    logout: {
-      marginTop: "12px",
     },
   });
 });
@@ -40,21 +41,24 @@ export default function Home() {
 
   return (
     <Container className={classes.container}>
-      <Typography variant="h4" classes={{ root: classes.header }}>
-        {username}
-      </Typography>
-      <Typography variant="h6" color="primary">
-        {status}
-      </Typography>
-      <Button
-        variant="contained"
-        color="primary"
-        size="large"
-        onClick={() => dispatch(logout())}
-        classes={{ root: classes.logout }}
-      >
-        Logout
-      </Button>
+      <Card variant="outlined" classes={{ root: classes.card }}>
+        <CardContent>
+          <Typography variant="h4" component="h1" color="primary">
+            {username}
+          </Typography>
+          <Typography color="textSecondary">{status}</Typography>
+        </CardContent>
+        <CardActions>
+          <Button
+            variant="outlined"
+            color="primary"
+            size="medium"
+            onClick={() => dispatch(logout())}
+          >
+            Logout
+          </Button>
+        </CardActions>
+      </Card>
     </Container>
   );
 }
